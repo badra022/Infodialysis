b6e3c99d9870eafd9dee0f5295801e664309cd8e
Ahmed Badra <42746335+badra069@users.noreply.github.com>
2021-01-07 01:07:46 +0200

    home, about basic structure

--- script.py
+++ script.py
@@ -1,13 +1,41 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+authors = [
+    {
+    'name':'Ahmed Badra',
+    'summary':''' 3rd year college student working towards a Bachelor in systems and biomedical engineering''',
+    'core_skills':['C', 'embedded systems', 'Autosar', 'OOP', 'c++', 'python', 'data structure', 'SQL']
+    },
+    {
+    'name':'Hassan Fathi',
+    'summary':''' 3rd year college student working towards a Bachelor in systems and biomedical engineering''',
+    'core_skills':['C', 'embedded systems', 'Autosar', 'OOP', 'c++', 'python', 'data structure', 'SQL']
+    },
+    {
+    'name':'Ammar Elsaeed',
+    'summary':''' 3rd year college student working towards a Bachelor in systems and biomedical engineering''',
+    'core_skills':['C', 'embedded systems', 'Autosar', 'OOP', 'c++', 'python', 'data structure', 'SQL']
+    },
+    {
+    'name':'Ahmed Sayed',
+    'summary':''' 3rd year college student working towards a Bachelor in systems and biomedical engineering''',
+    'core_skills':['C', 'embedded systems', 'Autosar', 'OOP', 'c++', 'python', 'data structure', 'SQL']
+    },
+    {
+    'name':'Youssef Mohamed',
+    'summary':''' 3rd year college student working towards a Bachelor in systems and biomedical engineering''',
+    'core_skills':['C', 'embedded systems', 'Autosar', 'OOP', 'c++', 'python', 'data structure', 'SQL']
+    }
+]
+
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World!</h1>'
+    return render_template("home.html")
 
 @app.route('/about')
 def about_function():
-    return '<h1>About!</h1>'
+    return render_template("about.html", authors=authors, title="About")
 
 if __name__ == "__main__":
     app.run(debug=True)
